bc1f9a38a704a41b21cd9af94c8fbad00a7142b2
Ajinkya Suryawanshi <ajinkyasuryawanshi20@gmail.com>
2023-10-29 20:12:00 +0530

    Add transfer list component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransferComponent } from './components/transfer/transfer.component';
 import { InputDirective } from './components/input/input.directive';
+import { TemplateLoaderService } from './components/transfer/transfer.service';
+import { HttpClientModule } from '@angular/common/http';
+import { TransferListComponent } from './components/transfer/transfer-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputDirective,
     TransferComponent,
+    TransferListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TemplateLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
